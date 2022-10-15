0014ecffa2405473570b74ab9c219020f24254a8
AsoreFox <julian.gil@live.com>
2022-10-15 17:52:59 -0300

    Codigo comentado eliminado en view.py

--- home/views.py
+++ home/views.py
@@ -5,24 +5,6 @@ from datetime import datetime
 import random
 from django.shortcuts import render
 
-
-# def crear_persona(request,nombre,apellido):
-#     persona= Persona(nombre=nombre, apellido=apellido, edad=random.randrange(1,99), fecha_creacion=datetime.now())
-#     persona.save()
-
-#     # template= loader.get_template("crear_persona.html")
-#     # template_renderizado= template.render({"personas": persona})
-#     return render(request, "home/crear_persona.html", {"personas": persona})
-
-# def ver_personas(request):
-#     personas= Persona.objects.all()
-#     # template=loader.get_template("ver_personas.html")
-#     # template_renderizado= template.render({"personas": personas})
-    
-#     return render(request, "home/ver_personas.html", {"personas": personas})
-
-
-# Armar las funciones, ya estan puestas estas funciones en urls.py 
 def cargar_libro(request):
     
     return render(request, "home/cargar_libro.html", {})
