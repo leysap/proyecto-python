f7c441a848f54c470a3182b517feb5e78d494f19
Leysa Pozo <leysapozo41@gmail.com>
2022-10-15 16:30:09 -0300

    creando clase en forms.py

--- home/forms.py
+++ home/forms.py
@@ -1,2 +1,8 @@
 from django import forms 
 
+class LibroFormulario(forms.Form):
+    nombre= forms.CharField(max_length=30)
+    descripcion= forms.CharField(max_length=50)    
+    categoria= forms.IntegerField(max_length=50)
+    precio= forms.DecimalField(max_digits=5, decimal_places=2)
+    fecha_creacion= forms.DateTimeField()
