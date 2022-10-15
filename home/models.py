02fefaa4a101c88716acf35f411dc51d2c5ba689
Leonardo <bonoleonardo@gmail.com>
2022-10-15 10:59:18 -0300

    Cambio test Leo

--- home/models.py
+++ home/models.py
@@ -5,3 +5,10 @@ class Persona(models.Model):
     apellido= models.CharField(max_length=30)
     edad= models.IntegerField()
     fecha_creacion= models.DateTimeField(null=True)
+    
+class Libros(models.Model): #Prueba Leo
+    nombre= models.CharField(max_length=30)
+    descripcion= models.CharField(max_length=50)
+    categoria= models.IntegerField(max_length=50)
+    precio= models.DecimalField(max_digits=5, decimal_places=2)
+    fecha_creacion= models.DateTimeField()
