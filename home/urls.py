1741b7b694d7aa4a47459d503c69106962a3fbb0
Leysa Pozo <leysapozo41@gmail.com>
2022-10-19 22:01:46 -0300

    crud y realizacion de clases basadas en vistas

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
+    #VERSION VISTAS
     path('', views.index, name="index"),
     path('cargar-libro/', views.cargar_libro, name='cargar_libro'),
     path('ver-libros/', views.ver_libros, name='ver_libros'),
+    path('editar/<int:id>', views.editar_libro, name='editar_libro'),
+    path('eliminar/<int:id>', views.eliminar_libro, name='eliminar_libro'),
     path('nosotros/', views.acerca_de_nosotros, name='acerca_de_nosotros'),
+       
+    #VERSION CLASES BASADAS EN VISTAS   
+    # path('ver-libros/', views.ListaLibros.as_view(), name='ver_libros'),
+    # path('cargar-libro/', views.CargarLibro.as_view(), name='cargar_libro'),
+    # path('editar/<int:pk>', views.EditarLibro.as_view(), name='editar_libro'),
+    # path('eliminar/<int:pk>', views.EliminarLibro.as_view(), name='eliminar_libro'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from home.forms import LibroFormulario, BusquedaLibroFormulario
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 @login_required
 def cargar_libro(request):
@@ -45,6 +47,48 @@ def ver_libros(request):
     formulario = BusquedaLibroFormulario()
     return render(request,"home/ver_libros.html", {'libros': libros, 'formulario': formulario})
 
+
+# funcion editar 
+@login_required
+def editar_libro(request,id):
+    libro= Libro.objects.get(id=id)
+    
+    if request.method=='POST':
+        formulario= LibroFormulario(request.POST)
+        if formulario.is_valid():
+            datos= formulario.cleaned_data
+            libro.nombre=datos['nombre']
+            libro.escritor=datos['escritor']
+            libro.descripcion=datos['descripcion']
+            libro.categoria=datos['categoria']
+            libro.precio=datos['precio']
+            libro.fecha_creacion=datos['fecha_creacion']
+            libro.save()
+            return redirect('ver_libros')
+        else:
+            return render(request, 'home/cargar_libro.html', {'libro': libro,'formulario': formulario})
+      
+    formulario= LibroFormulario(
+        initial={
+            'nombre': libro.nombre,
+            'escritor': libro.escritor,
+            'descripcion':libro.descripcion,
+            'categoria':libro.categoria,
+            'precio':libro.precio,
+            'fecha_creacion':libro.fecha_creacion
+            }
+        )
+      
+    return render(request, 'home/editar_libro.html', {'libro': libro,'formulario': formulario})
+
+#funcion eliminar
+@login_required
+def eliminar_libro(request,id):
+    libro= Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('ver_libros')
+
+
 @login_required
 def acerca_de_nosotros(request):
     
@@ -53,3 +97,30 @@ def acerca_de_nosotros(request):
 @login_required
 def index(request):
     return render(request, "home/index.html")
+
+
+
+#VERSION CLASES BASADAS EN VISTAS: - FALTA EL BUSCADOR-
+# class ListaLibros(ListView):
+#     model=Libro
+#     template_name='home/ver_libros.html'
+    
+    
+# class CargarLibro(CreateView):
+#     model= Libro
+#     template_name= 'home/cargar_libro.html'
+#     success_url= '/ver-libros/'
+#     fields= ['nombre', 'escritor', 'descripcion', 'precio', 'fecha_creacion']
+    
+    
+# class EditarLibro(UpdateView):
+#     model=Libro
+#     success_url= '/ver-libros/'
+#     template_name= 'home/editar_libro.html'
+#     fields= ['nombre', 'escritor', 'descripcion', 'precio', 'fecha_creacion']
+    
+    
+# class EliminarLibro(DeleteView):
+#     model=Libro
+#     success_url='/ver-libros/'
+#     template_name='home/eliminar_libro.html'
