82f9a2dbd55844c3a36fd4885310dca764e3d50f
Leysa Pozo <leysapozo41@gmail.com>
2022-10-20 17:10:29 -0300

    cambio en la urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('registrar/', views.registrar, name='registrar'),
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('perfil/', views.perfil, name='perfil'),
-    path('editar_perfil/editar', views.editar_perfil, name='editar_perfil'),
-    path('editar_perfil/cambiar-contrasenia', views.CambiarContrasenia.as_view(), name='cambiar_contrasenia'),
+    path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
+    path('perfil/cambiar-contrasenia/', views.CambiarContrasenia.as_view(), name='cambiar_contrasenia'),
 ]
